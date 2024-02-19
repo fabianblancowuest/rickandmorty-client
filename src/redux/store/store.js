f66b30021737c3ceea2893c30ca8d742a254342a
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2024-02-19 14:44:02 -0300

    redux thunk fixed

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,14 +2,9 @@
 // const store = createStore(rootReducer);
 // export default store;
 
-import {
-	legacy_createStore as createStore,
-	applyMiddleware,
-	compose,
-} from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
-// import rootReducer from "./reducer/reducer";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
