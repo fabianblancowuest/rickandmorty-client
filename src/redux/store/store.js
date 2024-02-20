d3222bb2fa8bd6e0b33ba81b81cba48564617ff5
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2024-02-19 23:05:24 -0300

    redux-thunk syntax error fixed

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,7 +3,7 @@
 // export default store;
 
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import rootReducer from "../reducers";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
