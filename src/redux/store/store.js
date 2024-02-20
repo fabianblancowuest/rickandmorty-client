f529c5da5031d3c0e935502af59b95d8d58f70a4
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2024-02-19 23:10:00 -0300

    redux thunk removed

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,12 +2,17 @@
 // const store = createStore(rootReducer);
 // export default store;
 
-import { createStore, applyMiddleware, compose } from "redux";
-import { thunk } from "redux-thunk";
+import {
+	legacy_createStore as createStore,
+	applyMiddleware,
+	compose,
+} from "redux";
+// import { thunk } from "redux-thunk";
 import rootReducer from "../reducers";
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+// const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(rootReducer);
 
 export default store;
